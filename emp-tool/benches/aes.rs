c2015b48e59e48a82c37cb90040d3f7f1820f5b9
Xiang Xie <xiexiangiscas@gmail.com>
2023-06-21 00:05:00 +0800

    original encrypt blocks

--- emp-tool/benches/aes.rs
+++ emp-tool/benches/aes.rs
@@ -1,12 +1,14 @@
+use aes::{Aes128Enc, cipher::{KeyInit, BlockEncrypt}};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use emp_tool::{block::Block, aes::Aes};
+use emp_tool::{aes::Aes, block::Block};
+use generic_array::GenericArray;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let x = rand::random::<Block>();
     let aes = Aes::new(&x);
     let blk = rand::random::<Block>();
-    let blks = rand::random::<[Block;8]>();
+    let blks = rand::random::<[Block; 8]>();
 
     c.bench_function("aes::new", move |bench| {
         bench.iter(|| {
@@ -25,6 +27,17 @@ fn criterion_benchmark(c: &mut Criterion) {
         let aes = Aes::new(&key);
         bench.iter(|| {
             black_box(aes.encrypt_blocks::<8>(&blks));
+        });
+    });
+
+    c.bench_function("original aes::encrypt_blocks::<8>", move |bench| {
+        let key = rand::random::<[u8;16]>();
+        let aes = Aes128Enc::new_from_slice(&key).unwrap();
+        let blks = rand::random::<[u8; 16]>();
+        let blks = GenericArray::from(blks);
+        let mut blks = [blks; 8];
+        bench.iter(|| {
+            black_box(aes.encrypt_blocks(&mut blks));
         });
     });
 }
